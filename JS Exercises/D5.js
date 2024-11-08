9c9762e78b65bd8a605e57e9d592bd69e8750583
Dan131195 <daniele.franceschini1395@gmail.com>
2024-11-08 15:00:40 +0100

    Esercizio settimana 2

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -61,27 +61,41 @@ const cars = [
   },
 ];
 
-for (let i = 0; i < cars.length; i++) {
-  let license = { licensePlate: "AA 000 BB" };
-  cars.push(license);
-}
-console.log(cars);
-
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
 
+cars.push({
+  brand: "BMW",
+  model: "Serie1",
+  color: "Yellow",
+  trims: ["city", "s-line"],
+});
+console.log(cars);
+
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop();
+}
+console.log(cars);
+
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
+
 const justTrims = [];
-justTrims.push(cars.trims);
+
+for (let i = 0; i < cars.length; i++) {
+  justTrims.push(cars[i].trims);
+}
 console.log(justTrims);
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+
+
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
@@ -89,6 +103,12 @@ console.log(justTrims);
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
+
+let i = 0;
+
+while (i < numericArray) {
+
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
